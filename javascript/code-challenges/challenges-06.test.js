4d183f31ef686f78725c1af81023fd68e9b8d0f8
Andrea Riley <andreariley@me.com>
2023-12-04 17:08:56 -0500

    challenge 3 passed

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -42,7 +42,7 @@ Write a function named addPurchases that, given an array of objects as input, us
 ------------------------------------------------------------------------------------------------ */
 
 const addPurchases = (arr) => {
-  // Solution code here...
+  return arr.reduce((total, purchased) => total + purchased.purchasePrice, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
