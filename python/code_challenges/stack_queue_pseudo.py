ddd3a188624811993a2b6ad3699dda08b5225c09
Andrea Riley <andreariley@me.com>
2024-01-23 13:37:45 -0500

    code added

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,21 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+
+    def __init__(self):
+        self.inbox = Stack()
+        self.outbox = Stack()
+
+    def enqueue(self, value):
+        self.inbox.push(value)
+
+    def dequeue(self):
+        while not inbox.is_empty():
+            popped = inbox.pop()
+            outbox.push(popped)
+
+    dequeued = outbox.pop()
+
+    while not outbox.is_empty():
+        popped = self.outbox.pop()
+        self.inbox.push(popped)
