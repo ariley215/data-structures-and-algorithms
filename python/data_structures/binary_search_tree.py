7b87c22daa6c2d3703c1f7cf7ad2a396505b6c42
Andrea Riley <andreariley@me.com>
2024-01-26 16:13:07 -0500

    binary tree tests passing

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,4 +1,4 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
@@ -6,10 +6,49 @@ class BinarySearchTree(BinaryTree):
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def add(self, value):
+        node = Node(value)
 
-    def some_method(self):
-        # method body here
-        pass
+        if self.root is None:
+            self.root = node
+
+        def traverse(node, add_node):
+            if node is None:
+                return
+            if add_node.value < node.value:
+                if node.left is None:
+                    node.left = add_node
+                else:
+                    traverse(node.left, add_node)
+            elif add_node.value > node.value:
+                if node.right is None:
+                    node.right = add_node
+                else:
+                    traverse(node.right, add_node)
+
+        traverse(self.root, node)
+
+
+# def add(self, value):
+#     node = Node(value)
+
+#     if self.root is None:
+#         self.root = node
+
+#     def traverse(node, node_to_add):
+#         if node is None:
+#             return
+
+#         if node_to_add.value < node.value:
+#             if node.left is None:
+#                 node.left = node_to_add
+#             else:
+#                 traverse(node.left, node_to_add)
+
+#         elif node_to_add.value > node.value:
+#             if node.right is None:
+#                 node.right = node_to_add
+#             else:
+#                 traverse(node.right, node_to_add)
+
+#     traverse(self.root, node)
