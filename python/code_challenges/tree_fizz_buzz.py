06a3f6a32f3486d19db59f1b9f4f7d378f16785b
Andrea Riley <andreariley@me.com>
2024-02-01 11:57:18 -0500

    time to turn in

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,26 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.kary_tree import KaryTree, Node, Queue
 
 
-def fizz_buzz_tree():
-    pass
+def fizz_buzz_tree(kary_tree):
+    if kary_tree.root is None:
+        return None
+
+    clone_root = Node(kary_tree.root.value, kary_tree.root.children.copy())
+
+    while Queue:
+        current, clone = Queue.pop(0)
+
+        if current.value % 3 == 0 and current.value % 5 == 0:
+            clone.value = "FizzBuzz"
+        if current.value % 3 == 0:
+            clone.value = "Fizz"
+        if current.value % 5 == 0:
+            clone.value = "Buzz"
+        else:
+            clone.value = str(current.value)
+
+    for i, child in enumerate(current.children):
+        clone_child = Node(None)
+        clone.children[i] = clone_child
+        Queue.append((child, clone_child))
+    return KaryTree(root=clone_root)
