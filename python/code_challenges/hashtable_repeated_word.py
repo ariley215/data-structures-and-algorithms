bba00f2e7154e39a81c1757f9cef71927775a5be
Andrea Riley <andreariley@me.com>
2024-02-20 11:16:40 -0500

    whiteboard and code finished

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,19 @@
 from data_structures.hashtable import Hashtable
 
 
-def first_repeated_word():
-    pass
+def first_repeated_word(string):
+    hashtable = Hashtable()
+    hashtable = {}
+    input_string_list = string.lower().split(" ")
+    for word in range(len(input_string_list)):
+      if input_string_list[word] in hashtable:
+        hashtable [input_string_list[word]] += 1
+      else:
+        hashtable [input_string_list[word]] = 1
+
+    for word in range(len(input_string_list)):
+      if hashtable[input_string_list[word]] > 1:
+        return input_string_list[word]
+    return "There are not repeating words in the string."
+
+
