8f2f3509d8cffea4f68c371c7c3cd4a16199df7c
Andrea Riley <andreariley@me.com>
2024-02-22 16:46:43 -0500

    code

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,12 @@
 from data_structures.hashtable import Hashtable
 
 
-def left_join():
-    pass
+def left_join(ht1, ht2):
+    output = []
+    for key, value in ht1.words():
+        synonym_pair = [key, value]
+        synonym_pair.append(ht2.get(key, None))
+        output.append(synonym_pair)
+    return output
+
+
